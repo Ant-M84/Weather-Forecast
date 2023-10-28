60d6c9a29a6b7da20b406bb5cef37c68d519ffbe
Ant_M84 <anthony.l.michel@gmail.com>
2023-10-28 12:32:07 -0400

    compiled README, revised js for history search, revised css styling

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,59 @@
 $(document).ready(function () {
 
-    weatherCards = document.querySelector("#weather-cards")
-    currentCity = document.querySelector("#city")
-    cityId = "";
+    weatherCards = document.querySelector("#weather-cards");
+    currentCity = document.querySelector("#city");
+    oldInput = document.querySelector("#input");
 
+    // Code to search based on user input //
     $("#searchBtn").on("click", function () {
         var input = $("#input").val();
-        var button = $("<button/>").text(input);
+        var button = $("<button id='historyBtn'>").text(input);
         $("#saved").append(button);
         getCity();
     });
 
+    // Code to allow search history to be refreshed //
+    $(document).on("click","#historyBtn", function () {
+        var newInput= (this).innerHTML;
+        var cityName = newInput;
+        var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=1&appid=d0fcce6012b326b116df7b9a8ea31123";
+
+        fetch(requestUrl)
+            .then (function (response) {
+            return response.json();
+        })
+        .then (function (data) {
+            var {name, lat, lon} = data[0];
+            getWeather(name, lat, lon);
+        });
+    });
+
+    // Code to clear page history //
     $(".clearBtn").on("click", function() {
-    localStorage.clear();
     location.reload();
     });
 
-    // Function to dynamically create the HTML elements to be documented
+    // Code to dynamically create the HTML elements to be documented //
     var createWeatherCard = (cityName, weatherItem, index) => {
         if (index === 0) {
             return `<ul class="city-card">
-                        <h1> ${cityName} (${weatherItem.dt_txt.split(" ")[0]})<img src="http://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="Weather Icon"></img></h1>
-                        <h3>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)} C</h3>
+                        <h2> ${cityName} (${weatherItem.dt_txt.split(" ")[0]})<img src="http://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="Weather Icon"></img></h2>
+                        <h3>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)} °C</h3>
                         <h3>Wind: ${weatherItem.wind.speed} M/S</h3>
                         <h3>Humidity: ${weatherItem.main.humidity} %</h3>
                     </ul>`;
         } else {
             return  `<ul class="col-sm card">
-                        <h4> (${weatherItem.dt_txt.split(" ")[0]})</h4>
-                        <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)} C</h6>
+                        <h5> (${weatherItem.dt_txt.split(" ")[0]})</h5>
+                        <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)} °CC</h6>
                         <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
                         <h6>Humidity: ${weatherItem.main.humidity} %</h6>
                         <img src="http://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="Weather Icon"></img>
                     </ul>`;
         };
     }
-    // Function to obtain city details then create a new array for weather data for a single date //
+
+    // Code to obtain city details then create a new array using details for a single date //
     var getWeather = (cityName, lat, lon) => {
         var weatherUrl = "http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=d0fcce6012b326b116df7b9a8ea31123";
 
@@ -44,18 +62,14 @@ $(document).ready(function () {
             return response.json();
             })
         .then (function (data) {
-            console.log(data);
             var dailyWeather = [];
-            (data.list).splice(0, 4);
             for (i = 0; i <(data.list).length; i += 7){ 
             dailyWeather.push(data.list[i]);
-            }
-            console.log(dailyWeather);  
-            
+            } 
             currentCity.innerHTML = "";
-            weatherCards.innerHTML = "";
+            weatherCards.innerHTML = "";        
             
-        // Function to document the HTML 
+        // Code to document the data dynamically to the DOM //
             dailyWeather.forEach((weatherItem, index) => {
                 if (index === 0) {
                 currentCity.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
@@ -64,8 +78,9 @@ $(document).ready(function () {
                 }
             });
         });
-    }    
-    // Function to obtain city name, latitude and longitude from GeoCode API to use in Open Weather API//
+    }   
+     
+    // Code to obtain city name, latitude and longitude from GeoCode API to use in OpenWeatherMap API//
     function getCity () {
         var cityName = $("#input").val();
         var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=1&appid=d0fcce6012b326b116df7b9a8ea31123";
@@ -75,10 +90,9 @@ $(document).ready(function () {
             return response.json();
         })
         .then (function (data) {
-            console.log(data);
             var {name, lat, lon} = data[0];
             getWeather(name, lat, lon);
         });
     };
-
+    
 });
